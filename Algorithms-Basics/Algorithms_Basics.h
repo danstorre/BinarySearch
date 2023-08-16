26d970d7befd1e86e2cbc63a4def9cdb4adf6777
Dan Torres <danstorre@gmail.com>
2023-08-16 15:07:00 -0400

    Initial Commit

---
+++ Algorithms-Basics/Algorithms_Basics.h
@@ -0,0 +1,18 @@
+//
+//  Algorithms_Basics.h
+//  Algorithms-Basics
+//
+//  Created by Daniel Torres on 8/16/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Algorithms_Basics.
+FOUNDATION_EXPORT double Algorithms_BasicsVersionNumber;
+
+//! Project version string for Algorithms_Basics.
+FOUNDATION_EXPORT const unsigned char Algorithms_BasicsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Algorithms_Basics/PublicHeader.h>
+
+
